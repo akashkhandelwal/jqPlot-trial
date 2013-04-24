a21d5c190782916bcc4879913d8d1a7356aca792
Akash Khadelwal <akash.khandelwal@jombay.com>
2013-04-24 16:35:23 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqplotApp::Application.config.secret_token = '3a505c4ca29b6fafc5051f83914345aa93d9fd0b9d6eb7693c99a6a60216594e163d51f45d863df4817223e6a1736f4533272ae4203f7013963b326f43ad1d00'
